71fa22f9fdc20750e18681f76c66e58f4b7f1a9e
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-12-01 17:48:35 +0200

    fix: update supabase creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://szvkledocaedpehtnftj.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6dmtsZWRvY2FlZHBlaHRuZnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NTUzNzgsImV4cCI6MjA3MzIzMTM3OH0.CfjOz01Ryl2eGzXvNk8GPmzTNliF0LgIUotvueGNjBc',
+  supabaseUrl: 'https://udlmiueknufypcndelms.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkbG1pdWVrbnVmeXBjbmRlbG1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MDIyNTUsImV4cCI6MjA4MDE3ODI1NX0.JEBu73GV4eTwVVZ64C0sJmctcipdVxuIyPmGwL_GNIM',
 };
